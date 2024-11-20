acf0b07f1e3a08286d9a36b2d744bcbf3f256bf1
amank55 <amankumarr2005ak@gmail.com>
2024-11-20 23:50:06 +0530

    Made Some Changes in ejs Files

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const path = require("path")
 const express = require("express")
 const mongoose = require("mongoose")
 const userRouter = require("./routes/user.js")
-const blogRouter = require("./routes/blog.js")
+const router1 = require("./routes/blog.js")
 const cookieParser = require("cookie-parser")
+
+const Blog = require("./models/blog.js")
+
 const { checkForAuthenticationCookie } = require("./middlewares/authentication.js")
 const app = express();
 const PORT = 8000
@@ -15,15 +18,19 @@ app.set("view engine", 'ejs')
 app.set("views", path.resolve("./views/partials"))
 
 app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie("token"))
-app.get("/", (req, res) => {
+app.use(express.static(path.resolve("./public/")))
+app.get("/", async (req, res) => {
+    const allBlogs = await Blog.find({})
     res.render("Home", {
         user : req.user,
+        blogs: allBlogs
     })
 })
 
 app.use("/user", userRouter)
-app.use("/blog", blogRouter)
+app.use("/blog",router1)
 app.listen(PORT, ()=> console.log(`Server Statrted SuccessFully !! ${PORT}`))
