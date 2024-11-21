15ee69515fa60a4cdfea07bb88bb347de731bead
amank55 <amankumarr2005ak@gmail.com>
2024-11-21 17:27:32 +0530

    Made Comments Schema

--- routes/blog.js
+++ routes/blog.js
@@ -22,6 +22,27 @@ router1.get("/add-new", (req, res) => {
   });
 });
 
+router1.get("/:id", async (req, res) => {
+      const blog = await Blog.findById(req.params.id).populate("createdBy")
+      console.log("blog", blog)
+      return res.render('blog',{
+        user: req.user,
+        blog,
+      })
+})
+
+router1.delete("/:id", async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id);
+    if (!blog) {
+      return res.status(404).send("Blog not found");
+    }
+    return res.status(200).send("Blog deleted successfully");
+  } catch (error) {
+    return res.status(500).send("An error occurred while deleting the blog");
+  }
+});
+
 router1.post("/",upload.single('coverImage') ,async (req, res) => {
     const {title, body} = req.body
     const blog = await Blog.create({
